ab2162f8e0d0c5dbb6aa6ab8d8862a6ca2e84479
lvaolin <lvaolin@foresee.com.cn>
2021-12-08 10:12:51 +0800

    理想汽车 Java财务核算模块专家岗 算法面试题 合并有序链表

---
+++ src/main/java/com/dhy/leetcode/linked/MergeLinkedList.java
@@ -0,0 +1,34 @@
+package com.dhy.leetcode.linked;
+
+import java.util.ArrayList;
+
+/**
+ * @Project fighting-core
+ * @Description 合并有序链表
+ *
+ * 输入：[{1,2},{3,4,5},{6}]
+ * 输出：{1,2,3,4,5,6}
+ *
+ * @Author lvaolin
+ * @Date 2021/12/8 上午10:06
+ */
+public class MergeLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        return null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
