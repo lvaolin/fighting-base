02afe64b29419ce881fd016b87515b074c1706db
dhy <414906978@qq.com>
2022-03-08 23:43:13 +0800

    resilience4j  高阶函数编程   函数式编程

--- src/main/java/com/dhy/proxy/jdk1/Client2.java
+++ src/main/java/com/dhy/proxy/jdk1/Client2.java
@@ -1,29 +1,29 @@
-package com.dhy.proxy.jdk1;
-
-import com.dhy.proxy.jdk1.target.Hello;
-import com.dhy.proxy.jdk1.target.IHello;
-
-import java.lang.reflect.Proxy;
-
-/**
- * @author lvaolin
- * @create 2020/4/13 5:56 PM
- */
-public class Client2 {
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        //------传统方式生成对象--------
-        IHello hello3 = new Hello();
-        hello3.say();
-        System.out.println("----------------");
-
-        //-----类反射生成对象---------
-        IHello hello2 = Hello.class.newInstance();
-        hello2.say();
-        System.out.println("----------------");
-
-        //-----jdk动态代理模式生成对象-----
-        IHello helloJdk = (IHello)  Proxy.newProxyInstance(Hello.class.getClassLoader(),Hello.class.getInterfaces(),new MyProxy(new Hello()));
-        helloJdk.say();
-        System.out.println("----------------");
-    }
-}
+//package com.dhy.proxy.jdk1;
+//
+//import com.dhy.proxy.jdk1.target.Hello;
+//import com.dhy.proxy.jdk1.target.IHello;
+//
+//import java.lang.reflect.Proxy;
+//
+///**
+// * @author lvaolin
+// * @create 2020/4/13 5:56 PM
+// */
+//public class Client2 {
+//    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+//        //------传统方式生成对象--------
+//        IHello hello3 = new Hello();
+//        hello3.say();
+//        System.out.println("----------------");
+//
+//        //-----类反射生成对象---------
+//        IHello hello2 = Hello.class.newInstance();
+//        hello2.say();
+//        System.out.println("----------------");
+//
+//        //-----jdk动态代理模式生成对象-----
+//        IHello helloJdk = (IHello)  Proxy.newProxyInstance(Hello.class.getClassLoader(),Hello.class.getInterfaces(),new MyProxy(new Hello()));
+//        helloJdk.say();
+//        System.out.println("----------------");
+//    }
+//}
---
+++ src/main/java/com/dhy/resilience4j/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * @Title package-info
+ * @Description
+ *
+ *Resilience4j 是一个 Java™ 的容错库
+ *
+ * Resilience4j是受 Netflix Hystrix 启发的轻量级容错库，但专为函数式编程而设计。
+ * Resilience4j 提供了高阶函数（装饰器），以通过断路器、速率限制器、重试或隔板来增强任何功能接口、
+ * lambda 表达式或方法引用。
+ * 您可以在任何功能接口、lambda 表达式或方法引用上堆叠多个装饰器。
+ * 优点是你可以只挑选你需要的装饰器组件，按需引入，不需要全部引入。
+ *
+ * @Author lvaolin
+ * @Date 2022/3/8 22:56
+ **/
+package com.dhy.resilience4j;
