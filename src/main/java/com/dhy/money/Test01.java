f28d1972c89c3f08cab9168b6748d86419002406
dahuangya <414906978@qq.com>
2023-03-16 09:15:54 +0800

    BigDecimal实践研究

--- src/main/java/com/dhy/money/Test01.java
+++ src/main/java/com/dhy/money/Test01.java
@@ -1,6 +1,9 @@
 package com.dhy.money;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * @Project fighting-core
@@ -13,7 +16,23 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println(new BigDecimal("1.2").subtract(new BigDecimal(0.01)));
         //1.18999999999999999979183318288278314867056906223297119140625
+        System.out.println(new BigDecimal("1.2").subtract(new BigDecimal(0.01),new MathContext(1)));
+        //1
+        System.out.println(new BigDecimal("1.2").subtract(new BigDecimal(0.01),new MathContext(2)));
+        //1.2
+        System.out.println(new BigDecimal("1.2").subtract(new BigDecimal(0.01),new MathContext(3)));
+        //1.19
+        System.out.println(new BigDecimal("1.2").subtract(new BigDecimal(0.01),new MathContext(3, RoundingMode.DOWN)));
+        //1.18
         System.out.println(new BigDecimal("1.2").subtract(new BigDecimal("0.01")));
         //1.19
+        new BigDecimal(1L);
+        new BigDecimal(1);
+        new BigDecimal(0.1);
+        new BigDecimal(0.01,new MathContext(2));
+        new BigDecimal(new BigInteger("100"));
+        new BigDecimal('a');
+        //new BigDecimal(new char[]{'a','b'});
+
     }
 }
