943bfaf5430947b8d817b072f1ae297a370d1e7f
dahuangya <414906978@qq.com>
2023-05-04 09:27:11 +0800

    Map源码研究

---
+++ src/main/java/com/dhy/collection/MapDemo.java
@@ -0,0 +1,20 @@
+package com.dhy.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @Project fighting-core
+ * @Description 主要用途描述
+ * @Author lvaolin
+ * @Date 2023/4/27 上午8:26
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+        Map map = new HashMap<String,String>();
+        ConcurrentMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+
+    }
+}
